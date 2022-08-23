c772e212e84fda97dfe1972b2ae729fb626e577a
Ashton Esparza <ashton.esparza@yahoo.com>
2022-08-22 21:09:37 -0400

    first commit

---
+++ helper-hardhat.config.js
@@ -0,0 +1,14 @@
+const networkConfig = {
+  4: {
+    name: "rinkeby",
+    vrfCoordinatorV2: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
+  },
+  5: {
+    name: "goerli",
+    vrfCoordinatorV2: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
+  },
+};
+
+const developmentChains = ["hardhat", "localhost"];
+
+module.exports = { networkConfig, developmentChains };
